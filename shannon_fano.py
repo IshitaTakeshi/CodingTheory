3e61d32614b1cedb1fefb85bedf9ceb536d2fa23
IshitaTakeshi <ishitah.takeshi@gmail.com>
2016-06-26 01:24:30 +0900

    Rename variables

--- shannon_fano.py
+++ shannon_fano.py
@@ -1,12 +1,12 @@
 from math import log2, ceil
 
 
-def split(symbols, sorted_keys):
+def split(probability, sorted_symbols):
     split_point = 1
     min_diff = float('inf')
-    for i in range(1, len(sorted_keys)-1):
-        left = sum(symbols[key] for key in sorted_keys[:i])
-        right = sum(symbols[key] for key in sorted_keys[i:])
+    for i in range(1, len(sorted_symbols)-1):
+        left = sum(probability[k] for k in sorted_symbols[:i])
+        right = sum(probability[k] for k in sorted_symbols[i:])
         diff = abs(left-right)
         if diff < min_diff:
             split_point = i
@@ -14,44 +14,45 @@ def split(symbols, sorted_keys):
     return split_point
 
 
-def sort_keys_by_value(symbols):
-    return sorted(symbols.keys(), key=lambda k: symbols[k], reverse=True)
+def sort_symbols_by_value(probability):
+    symbols = probability.keys()
+    return sorted(symbols, key=lambda k: probability[k], reverse=True)
 
 
-def assign_digit(code, keys, digit):
-    for key in keys:
-        code.setdefault(key, "")
-        code[key] += digit
+def assign_digit(code, symbols, digit):
+    for symbol in symbols:
+        code.setdefault(symbol, "")
+        code[symbol] += digit
     return code
 
-def shannon_fano_(symbols, sorted_keys, code={}):
-    if len(sorted_keys) == 1:
+def shannon_fano_(probability, sorted_symbols, code={}):
+    if len(sorted_symbols) == 1:
         return code
 
-    split_point = split(symbols, sorted_keys)
-    L = sorted_keys[split_point:]
-    R = sorted_keys[:split_point]
+    split_point = split(probability, sorted_symbols)
+    L = sorted_symbols[split_point:]
+    R = sorted_symbols[:split_point]
 
     assign_digit(code, L, "1")
-    code = shannon_fano_(symbols, L, code)
+    code = shannon_fano_(probability, L, code)
 
     assign_digit(code, R, "0")
-    code = shannon_fano_(symbols, R, code)
+    code = shannon_fano_(probability, R, code)
 
     return code
 
 
-def shannon_fano(symbols):
-    return shannon_fano_(symbols, sort_keys_by_value(symbols))
+def shannon_fano(probability):
+    return shannon_fano_(probability, sort_symbols_by_value(probability))
 
 
 def test_wikipedia():
     occurrences = {"A": 15, "B": 7, "C": 6, "D": 6, "E": 5}
-    symbols = {}
-    for key in occurrences.keys():
-        symbols[key] = occurrences[key] / sum(occurrences.values())
+    probability = {}
+    for symbol in occurrences.keys():
+        probability[symbol] = occurrences[symbol] / sum(occurrences.values())
 
-    code = shannon_fano(symbols)
+    code = shannon_fano(probability)
     expected1 = {"A": "00", "B": "01", "C": "10", "D": "110", "E": "111"}
     expected2 = {"A": "00", "B": "01", "C": "110", "D": "10", "E": "111"}
     assert(code == expected1 or code == expected2)
