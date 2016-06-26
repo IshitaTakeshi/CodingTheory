6677974efcdbd55f8b16a9460bdca0b7c6f105fe
IshitaTakeshi <ishitah.takeshi@gmail.com>
2016-06-26 16:02:06 +0900

    Calculate entropy, make the result more readable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from shannon_fano import shannon_fano
 from shannon_fano_elias import shannon_fano_elias
 from huffman import huffman
+from metric import entropy, average_code_length
+from prefixcode import isprefixcode
 from util import show
 
 
@@ -9,21 +11,42 @@ probability = {
     "E": 0.3, "F": 0.02, "G": 0.1, "H": 0.05
 }
 
-print("Shannon-Fano coding")
-print("-----------------------------")
-code = shannon_fano(probability)
-show(probability, code)
-print("")
 
-print("Shannon-Fano-Elias coding")
-print("-----------------------------")
-code = shannon_fano_elias(probability, sort_symbols=True)
-show(probability, code)
-print("")
+print("# Symbols\n")
+print("Symbol  Probability")
+print("-------------------")
+for symbol, p in probability.items():
+    print("{}       {}".format(symbol, p))
+print("\n")
 
 
-print("Huffman coding")
-print("-----------------------------")
-code = huffman(probability)
-show(probability, code)
+print("# Shannon-Fano coding\n")
+shannon_fano_code = shannon_fano(probability)
+show(shannon_fano_code, probability)
+isprefixcode(shannon_fano_code)
 print("")
+
+print("# Shannon-Fano-Elias coding\n")
+shannon_fano_elias_code = shannon_fano_elias(probability, sort_symbols=True)
+show(shannon_fano_elias_code, probability)
+isprefixcode(shannon_fano_elias_code)
+print("")
+
+print("# Huffman coding\n")
+huffman_code = huffman(probability)
+show(huffman_code, probability)
+isprefixcode(huffman_code)
+print("")
+
+
+print("Entropy of data                         : {:.3f} [bit]".format(
+      entropy(probability.values())))
+
+print("Average Shannon-Fano code length        : {:.3f} [bit]".format(
+      average_code_length(shannon_fano_code, probability)))
+
+print("Average Shannon-Fano-Elias code length  : {:.3f} [bit]".format(
+      average_code_length(shannon_fano_elias_code, probability)))
+
+print("Average Huffman code length             : {:.3f} [bit]".format(
+      average_code_length(huffman_code, probability)))
--- util.py
+++ util.py
@@ -1,8 +1,9 @@
 # The MIT License (MIT)
 # Copyright (c) 2016 Ishita Takeshi
 
-def show(probability, code):
+def show(code, probability):
     print("Symbol  Probability  Codeword")
+    print("-----------------------------")
     for symbol in sorted(probability.keys()):
         print("{}       {:.2f}         {}".format(
               symbol, probability[symbol], code[symbol]))
